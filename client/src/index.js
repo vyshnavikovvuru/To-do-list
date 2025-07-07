50bd93e184e0f6675cf213d8e094f5fccdef8544
Vyshnavi Kovvuru <137849211+vyshnavikovvuru@users.noreply.github.com>
2025-07-07 20:49:02 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-// client/src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import App from './Home'; // your to-do app
+import App from './Home'; 
 import Login from './Login';
 import Register from './Register';
 import './index.css';
